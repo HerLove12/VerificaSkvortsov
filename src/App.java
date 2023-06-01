f9eb4c8020a4dc155ebe216c6bccf7031a77e232
lai2-01 <lai2-01@itismeucci.com>
2023-06-01 14:34:42 +0200

    svolto esercizio bonus

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args){
-        Semaforo s = new Semaforo(1);
+        Semaforo s = new Semaforo(2);
         Box b = new Box();
 
         Macchina m1 = new Macchina(1,"Marco", s, b);
--- src/Macchina.java
+++ src/Macchina.java
@@ -14,10 +14,20 @@ public class Macchina extends Thread{
 
     @Override
     public void run(){
-        int pausaGiro = (int)Math.random()*4000+1000;
-        int pausaBox = (int)Math.random()*5000+1000;
 
         for(int i = 1; i <= 10; i++){
+            int pausaGiro = (int)(Math.random()*4000+1000);
+            int pausaBox = (int)(Math.random()*5000+1000);
+            int randomRitiro = (int)(Math.random()*20+1);
+
+            if(randomRitiro == 17){
+                System.out.println("Il pilota:  "+Thread.currentThread().getName()+" !HA ROTTO IL MOTORE!");
+                try {
+                    join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
             System.out.println("Il pilota:  "+Thread.currentThread().getName()+" sta svolgendo il " +i+" giro");
 
             try {
